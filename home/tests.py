a5d8b1a65c9231f4cd6619a0dab07bc6181a8af8
Cristian Sepulveda <air.enrill@gmail.com>
2015-03-31 15:11:54 -0300

    pagina home del sitio

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,13 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeView(TestCase):
+
+	def setUp(self):
+		self.url = reverse('home.views.home')
+
+	def test_get(self):
+		response = self.client.get(self.url)
+		self.assertEqual(200, response.status_code)
+		self.assertTemplateUsed(response, 'home/home.html')
---
+++ home/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('home.views',
+	url(r'^$', 'home', name='home'),
+)
